52c7e9d1dddbb48a8336d913c5845c3f4268636c
fireph <443370+fireph@users.noreply.github.com>
2026-05-12 17:04:43 -0700

    app patch to remove active now tray

---
+++ patches/src/main/kotlin/app/morphe/patches/messenger/inbox/HideFriendsInboxTrayPatch.kt
@@ -0,0 +1,21 @@
+/*
+ * Forked from:
+ * https://gitlab.com/ReVanced/revanced-patches/-/blob/main/patches/src/main/kotlin/app/revanced/patches/messenger/inbox/HideActiveNowTrayPatch.kt
+ */
+package app.morphe.patches.messenger.inbox
+
+import app.morphe.patches.shared.compat.AppCompatibilities
+import app.morphe.patcher.extensions.InstructionExtensions.replaceInstruction
+import app.morphe.patcher.patch.bytecodePatch
+
+@Suppress("unused")
+val hideFriendsInboxTrayPatch = bytecodePatch(
+    name = "Hide friends inbox tray",
+    description = "Hides the top user horizontal tray (Active Now and Notes) in the inbox.",
+) {
+    compatibleWith(AppCompatibilities.MESSENGER)
+
+    execute {
+        FriendsInboxTrayFingerprint.method.replaceInstruction(2, "const/4 v0, 0x0")
+    }
+}
